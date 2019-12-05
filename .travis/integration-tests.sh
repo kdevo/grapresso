8179e2ec36a8e949d524a6730b22f5447dcc838a
kdevo <kdevo@users.noreply.github.com>
2019-12-05 22:11:57 +0100

    :bug: Fix integration-tests.sh

--- .travis/integration-tests.sh
+++ .travis/integration-tests.sh
@@ -2,7 +2,7 @@
 
 set -ev
 git clone https://github.com/kdevo/grapresso-it.git --depth 1
-cp -f ./travis/inject-path__init__.py grapresso-it/tests/__init__.py
+cp -f .travis/inject-path__init__.py grapresso-it/tests/__init__.py
 cd grapresso-it/
 pip install -r requirements.txt
 pytest
